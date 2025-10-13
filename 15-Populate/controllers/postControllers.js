86ef3a436439a60f7325a7e1bc093b057e3d8036
DEEPAK-SINGH-BVM <deepaksingh.bvminfotech@gmail.com>
2025-10-13 17:54:19 +0530

    Update

--- 15-Populate/controllers/postControllers.js
+++ 15-Populate/controllers/postControllers.js
@@ -2,24 +2,32 @@ const { default: mongoose } = require("mongoose");
 const { Post } = require("../model/PostModel");
 
 const createPost = async (req, res) => {
-  const { title, postBy } = req.body;
+  try {
+    const { title, postBy } = req.body;
 
-  if (!title) {
-    return res.status(400).json({ message: "Title Required !!" });
+    if (!title) {
+      return res.status(400).json({ message: "Blog title Required !!" });
+    }
+    if (!postBy) {
+      return res.status(400).json({ message: "User Id Required" });
+    }
+    if (!mongoose.Types.ObjectId.isValid(postBy)) {
+      res.status(400).json({ message: "ID Not Match !!" });
+    }
+    const newPost = await Post.create({ title, postBy });
+    res.status(200).json(newPost);
+  } catch (error) {
+    res.json({ message: "Error in Create Post" });
   }
-  if (!postBy) {
-    return res.status(400).json({ message: "User Id Required" });
-  }
-  if (!mongoose.Types.ObjectId.isValid(postBy)) {
-    res.status(400).json({ message: "ID Not Match !!" });
-  }
-  const newPost = await Post.create({ title, postBy });
-  res.status(200).json(newPost);
 };
 
 const getAllPost = async (req, res) => {
-  const getPost = await Post.find().populate("postBy");
-  res.status(200).json(getPost);
+  try {
+    const getPost = await Post.find().populate("postBy");
+    res.status(200).json(getPost);
+  } catch (error) {
+    res.json({ message: "Error in Get Post" });
+  }
 };
 
 module.exports = { createPost, getAllPost };
--- 15-Populate/controllers/userControllers.js
+++ 15-Populate/controllers/userControllers.js
@@ -2,25 +2,33 @@ const { User } = require("../model/PostModel");
 
 const createUser = async (req, res) => {
   // destructure of validation
-  const { username, email } = req.body;
-  if (!username) {
-    return res.status(400).json({ message: "User Name is Required !!" });
-  }
-  if (!email) {
-    return res.status(400).json({ message: "Email Required !!" });
-  }
-  const isExist = await User.findOne({ email });
+  try {
+    const { username, email } = req.body;
+    if (!username) {
+      return res.status(400).json({ message: "User Name is Required !!" });
+    }
+    if (!email) {
+      return res.status(400).json({ message: "Email Required !!" });
+    }
+    const isExist = await User.findOne({ email });
 
-  if (isExist) {
-    return res.status(400).json({ message: "User Already Exists" });
+    if (isExist) {
+      return res.status(400).json({ message: "User Already Exists" });
+    }
+    const newUser = await User.create({ username, email });
+    res.status(200).json(newUser);
+  } catch (error) {
+    res.json({ message: "Error in Create User" });
   }
-  const newUser = await User.create({ username, email });
-  res.status(200).json(newUser);
 };
 
 const getAllUser = async (req, res) => {
-  const getUser = await User.find();
-  res.status(200).json(getUser);
+  try {
+    const getUser = await User.find();
+    res.status(200).json(getUser);
+  } catch (error) {
+    res.json({ message: "Error in Get user" });
+  }
 };
 
 module.exports = { createUser, getAllUser };
