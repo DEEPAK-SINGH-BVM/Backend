b5e45b031454e416911168addbd2d3f5427b95d3
DEEPAK-SINGH-BVM <deepaksingh.bvminfotech@gmail.com>
2025-10-17 17:46:08 +0530

    Update

--- 23-NodeMailer/index.js
+++ 23-NodeMailer/index.js
@@ -1,35 +1,3 @@
-// const express = require("express");
-// const nodemailer = require("nodemailer");
-// require("dotenv").config();
-
-// const app = express();
-// app.use(express.json());
-
-// const transporter = nodemailer.createTestAccount({
-//   service: "smtp.gmail.com",
-//   auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.PASS_USER,
-//   },
-// });
-
-// app.post("/sendEmail", async (req, res) => {
-//   const { to, subject, text } = req.body;
-
-//   const mailOption = {
-//     form: process.env.EMAIL_USER,
-//     to,
-//     subject,
-//     text,
-//   };
-
-//   const info = await transporter.sendMail(mailOption);
-//   res.json({ message: "Email Send Successfully !!", info });
-// });
-
-// app.listen(8090, () => {
-//   console.log("Server Start Successfully !!");
-// });
 import nodemailer from "nodemailer";
 import express from "express";
 import dotenv from "dotenv";
