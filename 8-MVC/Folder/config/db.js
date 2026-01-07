1492270a9b9fbd83c065761458373d6933f57755
DEEPAK-SINGH-BVM <deepaksingh.bvminfotech@gmail.com>
2026-01-07 09:34:35 +0530

    Changes

--- 8-MVC/Folder/config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const db = async ()=>{
-    await mongoose.connect("mongodb+srv://deepaksinghbvminfotech_db_user:XaXoeeGGRfPGZa33@cluster0.uop9z8c.mongodb.net/");
-    console.log('Connect to DataBase');
-} 
-
-module.exports = db;
